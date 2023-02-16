6f99448cf31cb45b425f9ea9be0f6862550aa843
Mathew <80817054+EmpRose@users.noreply.github.com>
2023-02-16 03:14:40 +0100

    Add files via upload

---
+++ Enumeration/EFruit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    internal enum EFruit
+    {
+        Apple = 0,
+        Banana = 1,
+        Watermelon = 2,
+        Orange = 3
+    }
+}
---
+++ Enumeration/Program.cs
@@ -0,0 +1,20 @@
+﻿using Enumeration;
+using System;
+namespace Enumaration
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Wich fruit do you like?");
+            foreach (EFruit item in Enum.GetValues(typeof(EFruit)))
+            {
+                Console.WriteLine(item + ":" + (int)item);
+            }
+
+            int userInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("I like {0}´s, too", (EFruit)userInput);
+
+        }
+    }
+}
